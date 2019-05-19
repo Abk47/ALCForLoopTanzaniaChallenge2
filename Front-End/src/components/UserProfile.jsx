84a1fe4d620ff5939852965099d943d9efa1a1fa
Abuka <maacsgaming@gmail.com>
2019-05-19 19:15:09 +0300

    add userprofile css

--- Front-End/src/components/UserProfile.jsx
+++ Front-End/src/components/UserProfile.jsx
@@ -9,8 +9,8 @@ class UserProfile extends Component {
     render() {
         return (
             <div className="UserProfile">
-                <img src="images/img.jpg" alt="John" style={{ width: "100%" }} />
-                <h1>John Doe</h1>
+                <img src="images/img.jpg" alt="John" />
+                <h1 className="full-name">John Doe</h1>
                 <p className="user-title">Software Developer, Andela</p>
                 <p><strong>Status:</strong> Available</p>
                 <p><strong>Number of rides given:</strong> 20 </p>
@@ -18,7 +18,7 @@ class UserProfile extends Component {
                 <a className="social-link" href="#"><i className="fab fa-twitter-square"></i></a>
                 <a className="social-link" href="#"><i className="fab fa-github-square"></i></a>
                 <a className="social-link" href="#"><i className="fab fa-facebook-square"></i></a>
-                <p><button className="user-button" onclick={this.handleClick}>Click to view list of rides</button></p>
+                <p><button className="user-button" onClick={this.handleClick}>Click to view list of rides</button></p>
             </div>
         );
     }
