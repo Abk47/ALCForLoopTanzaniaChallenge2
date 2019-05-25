498f8f387396e510d4626da8e069d87d86f31b61
Abuka <maacsgaming@gmail.com>
2019-05-25 16:19:13 +0300

    add ride-offer component

---
+++ Front-End/src/components/RideOffer.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './Info.css';
+
+class RideOffer extends Component{
+    render() {
+        return (
+            <div className="ride-request">
+                
+            </div>
+        );
+    }
+}
+
+export default RideOffer;
