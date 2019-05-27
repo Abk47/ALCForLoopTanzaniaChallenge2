1ac513b88848befb2a1bdd8484657e5a22d364b4
Abuka <maacsgaming@gmail.com>
2019-05-27 11:27:18 +0300

    add rideOffer component

--- Front-End/src/components/RideOffer.jsx
+++ Front-End/src/components/RideOffer.jsx
@@ -1,11 +1,16 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './Info.css';
 
-class RideOffer extends Component{
+class RideOffer extends Component {
     render() {
         return (
             <div className="ride-request">
-                
+                <h3>INCOMING RIDE REQUEST</h3>
+                <p>Ride offer details</p>
+                <div className="buttons">
+                    <button class="accept" type="submit">ACCEPT</button>
+                    <button class="reject" type="submit">REJECT</button>
+                </div>
             </div>
         );
     }
