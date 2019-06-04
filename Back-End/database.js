0cb2370b4f20d6844439ce92553f2010450e2bbd
Abuka <maacsgaming@gmail.com>
2019-06-04 12:10:52 +0300

    add create table functions

--- Back-End/database.js
+++ Back-End/database.js
@@ -9,10 +9,75 @@ const client = new Client({
 });
 
 client.connect()
-.then(() => {
-    console.log('connected to database');
-})
-.catch((err) => {
+  .then(() => {
+    console.log('connected to the database');
+    client.end();
+  })
+  .catch((err) => {
     console.log(err);
-})
-.finally(() => client.end());
+    client.end();
+  });
+
+
+const createUsersTable = () => {
+  const queryText = `CREATE TABLE IF NOT EXISTS
+      users(
+        id SERIAL PRIMARY KEY,
+        email VARCHAR(128) NOT NULL,
+        password VARCHAR(128) NOT NULL,
+        fname VARCHAR(128) NOT NULL,
+        lname VARCHAR(128) NOT NULL,
+        active numeric DEFAULT 0,
+        created_date TIMESTAMP NOT NULL DEFAULT NOW(),
+        modified_date TIMESTAMP NOT NULL DEFAULT NOW()
+      )`;
+
+  client
+    .query(queryText)
+    .then((res) => {
+      console.log(res);
+      client.end();
+    })
+    .catch((err) => {
+      console.log(err);
+      client.end();
+    });
+
+
+};
+
+const createRidesTable = () => {
+  const queryText = `CREATE TABLE IF NOT EXISTS
+        rides(
+          id SERIAL PRIMARY KEY,
+          name VARCHAR(128) NOT NULL,
+          age VARCHAR(128) NOT NULL,
+          destination VARCHAR(128) NOT NULL,
+          status VARCHAR(128) NOT NULL DEFAULT 0,
+          created_date TIMESTAMP NOT NULL DEFAULT NOW(),
+          modified_date TIMESTAMP NOT NULL DEFAULT NOW()
+        )`;
+
+  client
+    .query(queryText)
+    .then((res) => {
+      console.log(res);
+      client.end();
+    })
+    .catch((err) => {
+      console.log(err);
+      client.end();
+    });
+};
+
+// Create all Tables
+const createAllTables = () => {
+  createUsersTable();
+  createRidesTable();
+};
+
+createAllTables();
+
+module.exports = {
+  createAllTables,
+};
