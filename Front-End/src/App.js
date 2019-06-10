fb28032ac34019a942b171290e794c6c24540d4b
Abuka <maacsgaming@gmail.com>
2019-06-11 01:02:08 +0300

    Add route path

--- Front-End/src/App.js
+++ Front-End/src/App.js
@@ -1,23 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
-// import SignIn from './components/SignIn/SignIn';
-// import SignUp from './components/SignUp';
-// import UserProfile from './components/UserProfile';
+import SignIn from './components/SignIn/SignIn';
+import SignUp from './components/SignUp';
+import UserProfile from './components/UserProfile';
 import Sidebar from './components/Sidebar';
+// import RideOffer from './components/RideOffer';
 import Info from './components/Info';
 
-class App extends React.Component {
-  render(){
+function App() {
   return (
-    <div className="App">
-       {/* <SignUp />  */}
-       {/* <SignIn />      */}
-       {/* <UserProfile /> */}
-       <Sidebar />
-       <Info />
-    </div>
+    <Router>
+      <React.Fragment>
+        <Route exact path="/" component={Info} />
+        <Route exact path="/" component={Sidebar} />
+        <Route path="/user_profile" component={UserProfile} />
+        <Route path="/signUp" component={SignUp} />
+        <Route path="/signIn" component={SignIn} />
+      </React.Fragment>
+    </Router>
+
+
   );
-}
 }
 
 export default App;
