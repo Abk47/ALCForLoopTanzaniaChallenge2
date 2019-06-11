f592d7d05cd69842be64ddd870dd128f85ac2ce0
Abuka <maacsgaming@gmail.com>
2019-06-11 16:46:29 +0300

    add component and styling

--- Front-End/src/App.js
+++ Front-End/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Switch, Route } from 'react-router-dom';
 import './App.css';
 import SignIn from './components/SignIn/SignIn';
 import SignUp from './components/SignUp';
@@ -7,19 +7,24 @@ import UserProfile from './components/UserProfile';
 import Sidebar from './components/Sidebar';
 // import RideOffer from './components/RideOffer';
 import Info from './components/Info';
+import ViewRide from './components/ViewRide';
+
 
 function App() {
   return (
-    <Router>
-      <React.Fragment>
-        <Route exact path="/" component={Info} />
-        <Route exact path="/" component={Sidebar} />
-        <Route path="/user_profile" component={UserProfile} />
+    <React.Fragment>
+      <Switch>
+        <Route exact path="/" component={SignIn} />
+        <Route exact path="/dashboard" component={Sidebar} />
+        <Route exact path="/dashboard" component={Info} />
+        <Route exact path="/user" component={UserProfile} />
+        <Route exact path="/user" component={Sidebar} />
+        <Route exact path="/rides" component={Sidebar} />
+        <Route exact path="/rides" component={ViewRide} />
         <Route path="/signUp" component={SignUp} />
-        <Route path="/signIn" component={SignIn} />
-      </React.Fragment>
-    </Router>
-
+        {/* <Route component={error404} /> */}
+      </Switch>
+    </React.Fragment>
 
   );
 }
