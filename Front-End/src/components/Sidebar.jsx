ae40946a438a2de8fd55cadccc105f642be2e3bf
Abuka <maacsgaming@gmail.com>
2019-06-11 16:47:55 +0300

    add navlinks

--- Front-End/src/components/Sidebar.jsx
+++ Front-End/src/components/Sidebar.jsx
@@ -1,5 +1,6 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './SignIn/SignIn.css';
+import { NavLink } from 'react-router-dom';
 
 
 class Sidebar extends Component {
@@ -7,16 +8,14 @@ class Sidebar extends Component {
         const { foo } = this.props;
         return (
             <ul className="sidebar">
-                <li className="sidebar-brand"><button>Ride My Way</button></li>
-                <li><button className="link-button" onClick={foo} ><i class="fas fa-chalkboard-teacher"></i> Dashboard</button></li>
-                <li><button className="link-button" onClick={foo} ><i class="fas fa-hands-helping"></i> Support</button></li>
+                <NavLink to='/dashboard' style={{ textDecoration: 'none' }}><li className="sidebar-brand"><button>Ride My Way</button></li></NavLink>
+                <NavLink to='/user' style={{ textDecoration: 'none' }}><li><button className="link-button" onClick={foo} ><i class="fas fa-chalkboard-teacher"></i> Profile</button></li></NavLink>
+                <NavLink to='/rides' style={{ textDecoration: 'none' }}><li><button className="link-button" onClick={foo} ><i class="fas fa-car-side"></i> View Rides </button></li></NavLink>
                 <li><button className="link-button" onClick={foo} ><i class="fas fa-question"></i> About</button></li>
-                <li><button className="link-button" onClick={foo} ><i class="fas fa-sign-out-alt"></i> Logout</button></li>
+                <NavLink to='/' style={{ textDecoration: 'none' }}><li><button className="link-button" onClick={foo} ><i class="fas fa-sign-out-alt"></i> Logout</button></li></NavLink>
             </ul>
         );
     }
 }
 
 export default Sidebar;
-
-
